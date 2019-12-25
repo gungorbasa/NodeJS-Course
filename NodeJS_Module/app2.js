ba07c3d7314b95fb1d3eaae866d3eda489db62f0
Gungor Basa <gungor.basa@deliveryhero.com>
2019-12-25 09:28:46 +0300

    Parsing request

---
+++ NodeJS_Module/app2.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message!</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && req.method === 'POST') {
+        const body = []
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('./message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><h1>Hello World!</h1></html>');
+    res.end();
+}).listen(3000);
