608e20875025a70e17fa829c8716d905866f11c3
Gungor Basa <gungor.basa@deliveryhero.com>
2019-12-25 08:58:11 +0300

    Response object and sending first response back

--- NodeJS_Module/app.js
+++ NodeJS_Module/app.js
@@ -2,4 +2,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
     console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><h1>Hello World!</h1></html>');
+    res.end();
 }).listen(3000);
